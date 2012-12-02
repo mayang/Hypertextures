b410b39fd721c301d77b0d68af9b22ba0a681cae
may ang <mayday815@gmail.com>
2012-12-02 11:28:40 -0800

    corrections so you can test BasicNoise
    since it makes my lappy cry.

--- js/hypertextures.js
+++ js/hypertextures.js
@@ -1,10 +1,10 @@
 function BasicNoise(x, y, z) {
         f = 1; // TEMP
         var simplex = new SimplexNoise()
-        //var nfx = simplex.noise3D(x * f, y * f, z * f);
-        var nfx = simplex.noise2D(x*f, y*f);
+        var nfx = simplex.noise3D(x * f, y * f, z * f);
+        //var nfx = simplex.noise2D(x*f, y*f);
         console.log("nfx" + nfx);
-        return nfx;
+       // return nfx;
         var amp = 1 / f;
         var bump = (1 + amp*nfx);
         var xbump = x * bump;
