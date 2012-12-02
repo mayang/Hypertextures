6d4c50c749cc6f9da7d81e0cf2f59c8f4e360222
Michael Joyce <mltjoyce@gmail.com>
2012-12-02 12:06:58 -0800

    Variable clash fix

--- js/hypertextures.js
+++ js/hypertextures.js
@@ -1,11 +1,11 @@
 function BasicNoise(x, y, z) {
-        f = 1; // TEMP
+        var freq = 1; // TEMP
         var simplex = new SimplexNoise()
-        var nfx = simplex.noise3D(x * f, y * f, z * f);
+        var nfx = simplex.noise3D(x * freq, y * freq, z * freq);
         //var nfx = simplex.noise2D(x*f, y*f);
         console.log("nfx" + nfx);
        // return nfx;
-        var amp = 1 / f;
+        var amp = 1 / freq;
         var bump = (1 + amp*nfx);
         var xbump = x * bump;
         var ybump = y * bump;
