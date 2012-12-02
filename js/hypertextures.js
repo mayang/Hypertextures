8b141b83805495ae55a366a4f5eff8d69d89582d
mayang <mayday815@gmail.com>
2012-12-02 02:57:17 -0800

    so i tried it with this and it's soooo slow.
    no result yet. 

--- js/hypertextures.js
+++ js/hypertextures.js
@@ -5,10 +5,10 @@ function BasicNoise(x, y, z) {
         var nfx = simplex.noise2D(x*f, y*f);
         console.log("nfx" + nfx);
         return nfx;
-//        var amp = 1 / f;
-//        var bump = (1 + amp*nfx);
-//        var xbump = x * bump;
-//        var ybump = y * bump;
-//        var zbump = z * bump;
-//        return  xbump*xbump + ybump*ybump + zbump*zbump; 
+        var amp = 1 / f;
+        var bump = (1 + amp*nfx);
+        var xbump = x * bump;
+        var ybump = y * bump;
+        var zbump = z * bump;
+        return  xbump*xbump + ybump*ybump + zbump*zbump; 
 }
